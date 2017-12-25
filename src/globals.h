66848fd55f2c6e4c4f65414a2bf16f0fea81ef67
puzcoin <akuma@mud.com.cn>
2017-12-25 03:07:10 -0500

    change

--- src/globals.h
+++ src/globals.h
@@ -1,8 +1,8 @@
 #ifndef __GLOBALS_H__
 #define __GLOBALS_H__
 #define GENESIS_TIME_STAMP	"September 26, 2017, Alibaba Culture and Entertainment Group (Ali culture) announced the formal establishment of the game business group, under the open platform business and interactive entertainment division."
-#define DEFAULT_PORT 		22222
-#define DEFAULT_RPC_PORT	33333
+#define DEFAULT_PORT 		12222
+#define DEFAULT_RPC_PORT	12223
 #define GENESIS_BLOCK_TIME	1506425360
 #define GENESIS_BLOCK_NONCE	1526278	
 #define GENESIS_BLOCK_HASH	"0x0000085c71b5fad977230a1877ab0bdd7e6041b2c15246ecd336d8b8219913c1"
@@ -16,10 +16,8 @@
 #define MINING_REDUSE		300
 #define MINING_REDUSE_BLOCKS	50000
 #define MASTERNODE_COIN_NEEDED	3000000
-#define COIN_CONF_FILENAME	"puzcoin.conf"
+#define COIN_CONF_FILENAME	"dashero.conf"
 #define MASTERNODE_CONF_FILENAME	"masternode.conf"
-#define FORCE_MASTERNODE_PAYEE_BLOCKHEIGHT	15000
-#define FORK_15000_NBITS	0x1e0e0ff0
-#define FORK_15000_STEPS	100
+#define FORCE_MASTERNODE_PAYEE_BLOCKHEIGHT	2000
 #endif
 
